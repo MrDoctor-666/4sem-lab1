29379a8acefa697ef7ef25fcf7930c760c936cd6
MrDoctor-666 <58528229+MrDoctor-666@users.noreply.github.com>
2021-03-05 18:45:35 +0300

    Add files via upload

---
+++ 4sem1/4sem1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "RBtree.h"
+
+int main()
+{
+	RedBlackTree<int, char> t;
+	for (int i = 1; i < 7; i++) {
+		t.insert(i, (char)i+56);
+	}
+	t.remove(2);
+	/*t.insert(5, 'c');
+	t.insert(4, 'c');
+	t.insert(3, 'c');
+	t.insert(7, 'c');
+	t.insert(9, 'c');
+	t.insert(6, 'c');*/
+	t.print();
+	std::cout << t.find(3);
+}
